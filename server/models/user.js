1622b260d4e9b226eb810376149764eeed278718
dbz <dbzerjav@gmail.com>
2017-10-23 15:52:25 -0500

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -12,9 +12,7 @@ var UserSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator: (value)=>{
-
          return validator.isEmail(value);
-
        },
       message: '{VALUE} is not a valid email'
     }
@@ -52,6 +50,23 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 
